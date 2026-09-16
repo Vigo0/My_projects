71b9ee38a3d5bc09466cb73ab9a0b543aa6c87f3
user22 <Vladimirpopov003@bk.ru>
2026-09-16 12:17:33 +0300

    add page2

--- page1.cpp
+++ page1.cpp
@@ -2,5 +2,7 @@
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, niger" << std::endl;
+
     return 0;
 }
---
+++ page2.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main(){
+std::cout <<"bebroslav bebrilin"<< std::endl;
+return 0;
+}
